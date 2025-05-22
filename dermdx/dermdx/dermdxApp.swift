a6d192ccb6e84cd79477679eac90f61650630f5f
Cedric Nagata <cedric.nagata@gideontaylor.com>
2025-05-22 12:24:13 -0700

    basic iOS app

---
+++ dermdx/dermdx/dermdxApp.swift
@@ -0,0 +1,17 @@
+//
+//  dermdxApp.swift
+//  dermdx
+//
+//  Created by Cedric Nagata on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct dermdxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dermdx/dermdxTests/dermdxTests.swift
@@ -0,0 +1,17 @@
+//
+//  dermdxTests.swift
+//  dermdxTests
+//
+//  Created by Cedric Nagata on 5/22/25.
+//
+
+import Testing
+@testable import dermdx
+
+struct dermdxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
