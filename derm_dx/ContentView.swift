9e5be69f88b25dbb1c19f6d104d1be6557a48429
Cedric Nagata <cedric.nagata@gideontaylor.com>
2025-05-22 14:22:19 -0700

    basic iOS app

---
+++ derm_dx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  derm_dx
+//
+//  Created by Cedric Nagata on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
